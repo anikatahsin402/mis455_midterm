97312706e025ec16f3384111b15e3e83facc37cb
anikatahsin09 <2020934@iub.edu.bd>
2026-07-05 18:34:28 +0600

    Final project

---
+++ sscript.js
@@ -0,0 +1,11 @@
+function welcome() {
+    alert("Welcome to our Online Fashion Store!");
+}
+
+function buyNow() {
+    alert("Product added successfully!");
+}
+
+function contactMsg() {
+    alert("Thank you for contacting us!");
+}
